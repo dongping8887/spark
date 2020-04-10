9da6ff8c41ac785aabcd5e0cb9c9a8b0d8df6384
董平 <dp120945078>
2020-04-10 10:28:57 +0800

    调通测试方法

--- examples/src/main/scala/org/apache/spark/examples/sql/SparkSQLExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/sql/SparkSQLExample.scala
@@ -36,12 +36,12 @@ object SparkSQLExample {
     // $example on:init_session$
     val spark = SparkSession
       .builder()
+      .master("local[*]")
       .appName("Spark SQL basic example")
       .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
     // For implicit conversions like converting RDDs to DataFrames
-    import spark.implicits._
     // $example off:init_session$
 
     runBasicDataFrameExample(spark)
